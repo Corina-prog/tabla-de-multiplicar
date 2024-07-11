0ddd19883982992e2ce91584696f736af46406c3
Corina Mendoza <vivianmendoza07@hotmail.com>
2024-07-11 12:06:29 -0600

    proyecto iniciado

---
+++ src/ejerciciogit/EjercicioGit.java
@@ -0,0 +1,14 @@
+
+package ejerciciogit;
+
+public class EjercicioGit {
+  
+    public static void main(String[] args) {
+        System.out.println("La tabla de multiplicar de 10");
+       
+        for(int i = 0; i <=10; i++) {
+            System.out.println(10 + "x" + i + "=" + 10*i);
+            
+        }   
+        }
+    }
